bfbe55247c3792b0035b915859359140f4f8384e
noxas <dusson.romain@proton.me>
2023-09-25 22:42:38 +0200

    add nvchad config

---
+++ configs/lspconfig.lua
@@ -0,0 +1,21 @@
+local configs = require("plugins.configs.lspconfig")
+local on_attach = configs.on_attach
+local capabilities = configs.capabilities
+
+local lspconfig = require "lspconfig"
+
+lspconfig.html.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
+lspconfig.cssls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
+lspconfig.gopls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {"go"},
+}
