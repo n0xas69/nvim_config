c384265bf160ced60f1881e60460a1f0502666f3
n0xas <dusson.romain@proton.me>
2024-04-21 16:02:04 +0200

    change shortcut for go normal mode in insert mode

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,7 +5,7 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+map("i", "jj", "<ESC>")
 map("n", "lg", ":LazyGit<cr>")
 map("n", "<leader>e", ":NvimTreeToggle<cr>")
 
