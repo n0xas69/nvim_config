3679d8f3cc029c28e955d77c0d4183cebc1fe301
romain dusson <romain.dusson@alterway.fr>
2024-10-10 15:28:06 +0200

    change nvchad config for v3

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -8,7 +8,7 @@ local nvlsp = require "nvchad.configs.lspconfig"
 -- lsps with default config
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
-    on_attach = nvlsp.nvlsp.on_attach,
+    on_attach = nvlsp.on_attach,
     on_init = nvlsp.on_init,
     capabilities = nvlsp.nvlsp.capabilities,
   }
