422967d84d596e80e2a9d3253b4b0bfacb0cc671
romain dusson <romain.dusson@alterway.fr>
2024-10-10 15:25:34 +0200

    change nvchad config for v3

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,10 +2,16 @@ return {
   {
     "stevearc/conform.nvim",
     -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  {
+    "neovim/nvim-lspconfig",
     config = function()
-      require "configs.conform"
+      require "configs.lspconfig"
     end,
   },
+
   {
       "kdheepak/lazygit.nvim",
     	cmd = {
@@ -31,14 +37,8 @@ return {
     },
   },
 
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
   --
-  -- {
+-- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
   -- 		ensure_installed = {
