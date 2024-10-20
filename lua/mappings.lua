b4822be74332002f336a81292e818ddb4a62f198
n0xas <dusson.romain@proton.me>
2024-10-21 00:04:50 +0200

    change nvim tree shortcut

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,6 +7,6 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jj", "<ESC>")
 map("n", "<leader>lg", ":LazyGit<cr>")
-map("n", "<leader>e", ":NvimTreeToggle<cr>")
+map("n", "<leader>e", ":NvimTreeFocus<cr>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
